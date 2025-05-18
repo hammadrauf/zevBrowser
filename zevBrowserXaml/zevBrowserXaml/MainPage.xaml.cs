a5abcc364f26a79af106a01581a9a75485d5520f
hammadrauf <rauf.hammad@gmail.com>
2025-05-18 19:19:49 -0400

    OK to run. But Rebuild shows 1 (platform?) error (due to SDK updates?) (New Solution/Project will fix it). Error: The type 'UIElement' is defined in an assembly that is not referenced. You must add a reference to assembly 'Uno.UI, Version=255.255.255.255, Culture=neutral, PublicKeyToken=null'.

--- zevBrowserXaml/zevBrowserXaml/MainPage.xaml.cs
+++ zevBrowserXaml/zevBrowserXaml/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 using Windows.Storage.Pickers;
 using Windows.UI.Popups;
+using GridScroller.Package;
+using System.Collections.ObjectModel;
 
 
 namespace zevBrowserXaml;
@@ -9,6 +11,14 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+        this.gsDates.Items = new ObservableCollection<UIElement>
+        {
+            new CheckBox { Content = "2025-05-12" },
+            new CheckBox { Content = "2025-05-13" },
+            new CheckBox { Content = "2025-05-14" },
+            new Button { Content = "A" },
+            new Button { Content = "B" }
+        };
     }
 
     private async void onClick_btnFolder(object sender, RoutedEventArgs e)
